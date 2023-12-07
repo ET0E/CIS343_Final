e3eda53e9c12800015cd56690571d9f4da65762e
ET0E <ingramrt@mail.gvsu.edu>
2023-12-06 21:39:56 -0500

    Fuck vs code

--- Guess_The_number.js
+++ Guess_The_number.js
@@ -1,1 +1,23 @@
+function guess_number(number){
 
+        var random = (Math.random() * 100)+1;
+    
+        if(random == number){
+            console.log("Your guess was correct!");
+            running = false;
+        }
+
+        if(random < number){
+            console.log("Your guess was too high.");
+        }
+
+        if(random > number){
+            console.log("Your guess was too low.");
+        }
+    }
+
+number = parseInt(prompt('Guess a number 1 - 100'));
+
+while(running == true){
+    guess_number(number);
+}
